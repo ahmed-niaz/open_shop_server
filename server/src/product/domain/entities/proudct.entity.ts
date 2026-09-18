567813b07a2f9c74c5a0d72d14d3d72fcbb8f320
ahmed-niaz <inestnetwork@gmail.com>
2026-09-18 23:37:32 +0600

    feat:implemented the product entity

--- server/src/product/domain/entities/proudct.entity.ts
+++ server/src/product/domain/entities/proudct.entity.ts
@@ -1,3 +1,120 @@
 import { AggregateRoot } from '../../../shared/domain/aggregate-root.js';
+import { Money } from '../../../shared/domain/value-objects/money.vo.js';
+import { ProudctId } from '../value-objects/product-id.vo.js';
+import { SKU } from '../value-objects/sku.vo.js';
 
-export class Product extends AggregateRoot {}
+export interface ProudctProps {
+  id: ProudctId;
+  name: string;
+  price: Money;
+  description: string;
+  sku: SKU;
+  stock: number;
+  isActivate: boolean;
+  lowStockThreshold: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Product extends AggregateRoot {
+  private _id: ProudctId;
+  private _name: string;
+  private _price: Money;
+  private _description: string;
+  private _sku: SKU;
+  private _stock: number;
+  private _isActivate: boolean;
+  private _lowStockThreshold: number;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+
+  private constructor(props: ProudctProps) {
+    super();
+    this._id = props.id;
+    this._name = props.name;
+    this._price = props.price;
+    this._description = props.description;
+    this._sku = props.sku;
+    this._stock = props.stock;
+    this._isActivate = props.isActivate;
+    this._lowStockThreshold = props.lowStockThreshold;
+    this._createdAt = props.createdAt;
+    this._updatedAt = props.updatedAt;
+  }
+
+  //   create method
+  static create(
+    name: string,
+    description: string,
+    sku: string,
+    price: Money,
+    stock: number,
+  ) {
+    Product.validateName(name);
+    Product.validateStock(stock);
+
+    const now = new Date();
+
+    return new Product({
+      id: new ProudctId(),
+      name,
+      description,
+      sku: SKU.create(sku),
+      price,
+      stock,
+      isActivate: true,
+      lowStockThreshold: 10,
+      createdAt: now,
+      updatedAt: now,
+    });
+  }
+
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+  get sku() {
+    return this._sku;
+  }
+  get price() {
+    return this._price;
+  }
+  get stock() {
+    return this._stock;
+  }
+  get isActivate() {
+    return this._isActivate;
+  }
+  get lowStockThreshold() {
+    return this._lowStockThreshold;
+  }
+  get createdAt() {
+    return this._createdAt;
+  }
+  get updatedAt() {
+    return this._updatedAt;
+  }
+
+  //   method reconstitue
+  static reconstitute(props: ProudctProps): Product {
+    return new Product(props);
+  }
+
+  private static validateName(name: string): void {
+    if (name.length < 3) {
+      throw new Error('Name must be at least 3 characters');
+    }
+  }
+
+  private static validateStock(stock: number): void {
+    if (stock < 0) {
+      throw new Error('Stock cannot be negative');
+    }
+  }
+}
